778b3ebfdf0a0418c0767c5c9e2efb728fa45d99
MyratJr <begmyradowmyratgeldi@gmail.com>
2024-11-28 16:49:31 +0500

    add

--- Client/admin.py
+++ Client/admin.py
@@ -1,3 +1,107 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
+from .models import Advertisement, Group, Elevator
+from django.utils.safestring import mark_safe
+import pathlib
+from django.utils.html import format_html
+import unfold
+from django.utils import timezone
 
-# Register your models here.
+
+class ElevatorAdmin(unfold.admin.StackedInline):
+    model = Elevator
+    readonly_fields = ["last_connection", "date_created"]
+
+
+@admin.register(Advertisement)
+class AdvertisementAdmin(ModelAdmin):
+    def image_preview(self, obj):
+        if obj.media:
+            media_format = pathlib.Path(obj.media.url).suffix
+            if media_format in [".jpg", ".JPG", ".png", ".PNG", ".gif", ".GIF", ".jpeg", ".JPEG", ".bmp", ".BMP", ".tiff", ".TIFF", ".svg", ".SVG", ".ai", ".AI", ".eps", ".EPS", ".webp", ".WEBP", ".heif", ".HEIF", ".avif", ".AVIF"]:
+                return mark_safe('<img src="%s" />' % obj.media.url)
+            elif media_format in [".mp4", ".MP4", ".avi", ".AVI", ".mov", ".MOV", ".flv", ".FLV", ".mkv", ".MKV", ".wmv", ".WMV", ".webm", ".WEBM", ".vob", ".VOB", ".3gp", ".3GP", ".hevc", ".HEVC"]:
+                return mark_safe('<video style="height:300px;margin: 0 auto;" controls loop><source src="%s" type="video/mp4"> </video>' % obj.media.url)
+        return "Медиафайлы не могут быть видны."
+    image_preview.short_description = "Предварительный просмотр медиа:"
+    readonly_fields = ('image_preview',"date_created")
+
+    def get_fields(self, request, obj=None):
+        fields = super().get_fields(request, obj)
+        if obj is None:  # If adding a new object
+            fields.remove('image_preview')
+            fields.remove('date_created')
+        return fields
+    search_fields = ["parent__name", "title"]
+    
+
+@admin.register(Group)
+class GroupAdmin(ModelAdmin):
+    # inlines = [ElevatorAdmin, AdvertisementAdmin]
+    search_fields = ["name"]
+
+
+@admin.register(Elevator)
+class ElevatorAdmin(ModelAdmin):
+    list_display = ('name', "colored_status_counter", "time_status")
+    ordering = ["last_connection"]
+    readonly_fields = ["last_connection", "date_created"]
+    search_fields = ["parent__name", "name"]
+
+    def time_status(self, obj):
+        now = timezone.now()
+        max_value = 60
+        percentage = 0
+        if obj.last_connection:
+            time_diff = now - obj.last_connection
+            minutes = int(time_diff.total_seconds() / 60)
+            hours = 0
+            percentage = min(minutes / max_value, 1)
+
+            if minutes > 60:
+                hours = int(minutes / 60)
+                minutes = minutes % 60
+        else:
+            minutes = 0
+        
+        red = int(255 * percentage)
+        green = int(255 * (1 - percentage))
+        color = f'rgb({red}, {green}, 0)'
+
+        if hours == 0 and minutes <= 5:
+            return format_html(
+                '<strong style="color: {};">Все в порядке</strong>',
+                color
+            )
+        return format_html(
+            '<strong style="color: {};">Нет связи уже {} часов {} минут.</strong>',
+            color, hours, minutes
+        )
+
+    time_status.short_description = 'Статус соединения'
+   
+    def colored_status_counter(self, obj):
+        now = timezone.now()
+        if obj.last_connection:
+            time_diff = now - obj.last_connection
+            minutes = int(time_diff.total_seconds() / 60)
+        else:
+            minutes = 0
+        max_value = 60
+        percentage = min(minutes / max_value, 1)
+        
+        red = int(255 * percentage)
+        green = int(255 * (1 - percentage))
+        color = f'rgb({red}, {green}, 0)'
+        return format_html(
+            '<div style="width: 20px; height: 20px; border-radius: 50%; background-color: {}; display: inline-block;"></div>',
+            color
+        )
+    
+    colored_status_counter.short_description = 'Статус'
+    def get_fields(self, request, obj=None):
+        fields = super().get_fields(request, obj)
+        if obj is None:  # If adding a new object
+            fields.remove('last_connection')
+            fields.remove('date_created')
+        return fields
--- Client/models.py
+++ Client/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from django.utils import timezone
+import mimetypes
 
-# Create your models here.
+
+class Group(models.Model):
+    name = models.CharField(max_length=55, verbose_name="Название группы")
+    date_created = models.DateTimeField(auto_now_add=True, blank=True, null=True, verbose_name="Дата создания")
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ["-date_created"]
+        verbose_name = "Группа"
+        verbose_name_plural = "Группы"
+    
+
+class Elevator(models.Model):
+    parent = models.ForeignKey(Group, on_delete=models.SET_NULL, related_name="elevators", blank=True, null=True, verbose_name="Связанная группа")
+    name = models.CharField(max_length=55, verbose_name="Название")
+    last_connection = models.DateTimeField(blank=True, null=True, verbose_name="Последнее соединение")
+    date_created = models.DateTimeField(auto_now_add=True, blank=True, null=True, verbose_name="Дата создания")
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):        
+        if not self.pk:
+            self.last_connection = timezone.now()
+        super().save(*args, **kwargs)
+
+    class Meta:
+        ordering = ["-date_created"]
+
+    class Meta:
+        verbose_name = "Лифт"
+        verbose_name = "Лифты"
+
+
+class Advertisement(models.Model):
+    parent = models.ForeignKey(Group, on_delete=models.SET_NULL, related_name="advertisements", blank=True, null=True, verbose_name="Связанная группа")
+    title = models.CharField(max_length=555, verbose_name="Заголовок")
+    description = models.TextField(verbose_name="Описание")
+    media = models.FileField(upload_to="admin_A", verbose_name="Реклама")
+    filetype = models.CharField(verbose_name="Тип файла:", max_length=50, blank=True)
+    duration = models.IntegerField(verbose_name="Продолжительность в секундах:", default=0)
+    date_created = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+    def save(self, *args, **kwargs):
+        if self.media:
+            mime_type, _ = mimetypes.guess_type(self.media.name)
+            self.filetype = mime_type or 'unknown'
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = "Реклама"
+        verbose_name_plural = "Реклама"
