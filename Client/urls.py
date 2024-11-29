05296a1e91025e7fef5e155d2daf5bc67a04bec9
MyratJr <begmyradowmyratgeldi@gmail.com>
2024-11-29 17:48:06 +0500

    ru

--- Client/urls.py
+++ Client/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("advertisements/", views.AdvertisementAPIView.as_view({"get":"list"}), name="advertisements"), 
+    path("list-advertisements/", views.AdvertisementAPIView.as_view({"get":"list"}), name="advertisements"), 
     path('tablet-update-connection/<int:pk>/', views.ElevatorViewSet.as_view({'get': 'retrieve'}), name='tablet_connection_update'),
-    path('elevators/<int:elevator_id>/advertisements/', views.AdvertisementByElevatorAPIView.as_view()),
+    path('elevator-advertisements/<int:elevator_id>', views.AdvertisementByElevatorAPIView.as_view()),
 ]
