981a5252325a547fd71768fe2a055667e9ac13f9
MyratJr <begmyradowmyratgeldi@gmail.com>
2024-12-02 21:58:35 +0500

    logo

--- ElevatorProject/settings.py
+++ ElevatorProject/settings.py
@@ -160,8 +160,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = 'media/'
 
